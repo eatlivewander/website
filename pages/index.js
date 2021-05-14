fa87de5e239a08039fbf1066b607dfc05bce69c2
Victor Amos <victoramosjr@gmail.com>
2021-05-14 02:05:55 -0400

    add new post

--- pages/index.js
+++ pages/index.js
@@ -7,8 +7,93 @@ import SocialPostCard from '../components/SocialPostCard';
 
 const data = [
     {
+        postURL: 'https://www.tiktok.com/@eatlivewander/video/6961495354219056389',
+        imageURL:
+            'https://wndr.click/wp-content/uploads/2021/05/myxen-vixen-woodford-reserve-bourbon-whiskey-spiked-lemonade-cocktail-tutorial-recipe.jpg',
+        imageAlt: 'Myxen Vixen Woodford Reserve Bourbon Whiskey Spiked Lemonade Cocktail Tutorial Recipe',
         social: [
-            { name: 'facebook', posted: false, url: null },
+            { name: 'facebook', posted: false, url: '' },
+            { name: 'instagram', posted: false, url: 'https://www.instagram.com/p/COyRakGhL2n/' },
+            { name: 'pinterest', posted: true, url: 'https://pin.it/5xUK2in' },
+            { name: 'tiktok', posted: false, url: 'https://www.tiktok.com/@eatlivewander/video/6961495354219056389' },
+        ],
+        products: [
+            {
+                name: 'HALM 6-Inch Reusable Glass Cocktail Straws',
+                store: 'Amazon',
+                buyURL: 'https://amzn.to/3odsqr9',
+                imageURL:
+                    'https://wndr.click/wp-content/uploads/2021/05/HALM-6-Inch-Reusable-Glass-Cocktail-Straws.jpg',
+                imageAlt: 'HALM 6-Inch Reusable Glass Cocktail Straws',
+                price: '$14.90',
+            },
+            {
+                name: 'Brixton 12-Ounce Rocks Glass',
+                store: 'Crate & Barrel',
+                buyURL: 'https://www.crateandbarrel.com/brixton-12-ounce-rocks-glass/s335564',
+                imageURL: 'https://wndr.click/wp-content/uploads/2021/05/Crate-Barrel-Brixton-12-Ounce-Rocks-Glass.jpg',
+                imageAlt: 'Brixton 12-Ounce Rocks Glass',
+                price: '$4.99',
+            },
+            {
+                name: 'GasOne GS-8300 Portable Foldable Camp Stove',
+                store: 'Amazon',
+                buyURL: 'https://amzn.to/3f9cthD',
+                imageURL:
+                    'https://wndr.click/wp-content/uploads/2021/05/GasOne-GS-8300-Portable-Foldable-Camp-Stove.jpg',
+                imageAlt: 'GasOne GS-8300 Portable Foldable Camp Stove',
+                price: '$49.99',
+            },
+            {
+                name: 'hOmeLabs Chill Pill Portable Countertop Ice Maker',
+                store: 'Amazon',
+                buyURL: 'https://amzn.to/2RMiQ2d',
+                imageURL:
+                    'https://wndr.click/wp-content/uploads/2021/05/hOmeLabs-Chill-Pill-Portable-Countertop-Ice-Maker.jpg',
+                imageAlt: 'hOmeLabs Chill Pill Portable Countertop Ice Maker',
+                price: '$124.99',
+            },
+            {
+                name: 'Alessi Juicy Salif Citrus Squeezer',
+                store: 'Amazon',
+                buyURL: 'https://amzn.to/3aTOBNq',
+                imageURL:
+                    'https://wndr.click/wp-content/uploads/2021/05/Alessi-Philippe-Starck-Juicy-Salif-Citrus-Juicer-Squeezer-Design-Icon.jpg',
+                imageAlt: 'Alessi Philippe Starck Juicy Salif Citrus Juicer Squeezer Design Icon',
+                price: '$62.00',
+            },
+            {
+                name: '16-Ounce Glass Swing Top Bottles',
+                store: 'Amazon',
+                buyURL: 'https://amzn.to/3f3lQPQ',
+                imageURL: 'https://wndr.click/wp-content/uploads/2021/05/16-Ounce-Glass-Swing-Top-Bottles.jpg',
+                imageAlt: '16-Ounce Glass Swing Top Bottles',
+                price: '$19.99',
+            },
+            {
+                name: '3-In-1 Stainless Steel Small Funnel Set',
+                store: 'Amazon',
+                buyURL: 'https://amzn.to/3xZTDBW',
+                imageURL: 'https://wndr.click/wp-content/uploads/2021/05/3-In-1-Stainless-Steel-Small-Funnel-Set.jpg',
+                imageAlt: '3-In-1 Stainless Steel Small Funnel Set',
+                price: '$9.99',
+            },
+            {
+                name: 'Karter Scientific Glass Beaker Set (5-Sizes, 50ml - 1000ml)',
+                store: 'Amazon',
+                buyURL: 'https://amzn.to/2RA12Y0',
+                imageURL: 'https://wndr.click/wp-content/uploads/2021/05/Karter-Scientific-Glass-Beaker-Set.jpg',
+                imageAlt: 'Karter Scientific Glass Beaker Set (5-Sizes, 50ml - 1000ml)',
+                price: '$19.99',
+            },
+        ],
+    },
+    {
+        postURL: 'https://www.tiktok.com/@eatlivewander/video/6961495354219056389',
+        imageURL: 'https://wndr.click/wp-content/uploads/2021/05/Gruyere-And-Pepper-Jack-Gougeres-Recipe.png',
+        imageAlt: 'Gruyere And Pepper Jack Gougeres Recipe',
+        social: [
+            { name: 'facebook', posted: true, url: 'https://fb.watch/5spKvYbjux/' },
             { name: 'instagram', posted: true, url: 'https://www.instagram.com/p/COyRakGhL2n/' },
             { name: 'pinterest', posted: true, url: 'https://pin.it/5eH0gZC' },
             { name: 'tiktok', posted: true, url: 'https://www.tiktok.com/@eatlivewander/video/6961495354219056389' },
@@ -106,13 +191,13 @@ const data = [
                 price: '$11.99',
             },
         ],
-        postURL: 'https://www.tiktok.com/@eatlivewander/video/6961495354219056389',
-        imageURL: 'https://wndr.click/wp-content/uploads/2021/05/Gruyere-And-Pepper-Jack-Gougeres-Recipe.png',
-        imageAlt: 'Gruyere And Pepper Jack Gougeres Recipe',
     },
     {
+        postURL: 'https://www.tiktok.com/@eatlivewander/video/6960315675256671493',
+        imageURL: 'https://wndr.click/wp-content/uploads/2021/05/Earl-Grey-Sweet-Iced-Tea-Cocktail-Recipe-Tutorial.jpg',
+        imageAlt: 'Earl Grey Sweet Iced Tea Cocktail Recipe Tutorial',
         social: [
-            { name: 'facebook', posted: false, url: null },
+            { name: 'facebook', posted: true, url: 'https://fb.watch/5spThje0Tw/' },
             { name: 'instagram', posted: true, url: 'https://www.instagram.com/tv/COqAbKABiOX/' },
             { name: 'pinterest', posted: true, url: 'https://pin.it/ugVni5r' },
             { name: 'tiktok', posted: true, url: 'https://www.tiktok.com/@eatlivewander/video/6960315675256671493' },
@@ -211,13 +296,14 @@ const data = [
                 price: '$62.00',
             },
         ],
-        postURL: 'https://www.tiktok.com/@eatlivewander/video/6960315675256671493',
-        imageURL: 'https://wndr.click/wp-content/uploads/2021/05/Earl-Grey-Sweet-Iced-Tea-Cocktail-Recipe-Tutorial.jpg',
-        imageAlt: 'Earl Grey Sweet Iced Tea Cocktail Recipe Tutorial',
     },
     {
+        postURL: 'https://www.tiktok.com/@eatlivewander/video/6956326890739223813',
+        imageURL:
+            'https://wndr.click/wp-content/uploads/2021/04/How-To-Cut-And-Maintain-Fresh-Flowers-And-Roses-For-A-Diptqyue-Baies-Black-Vase-small.jpg',
+        imageAlt: 'How to Cut and Maintain Roses In A Vase',
         social: [
-            { name: 'facebook', posted: false, url: null },
+            { name: 'facebook', posted: true, url: 'https://fb.watch/5spWKK4RKW/' },
             { name: 'instagram', posted: true, url: 'https://www.instagram.com/tv/COWD4S-nPJZ' },
             { name: 'pinterest', posted: true, url: 'https://pin.it/7z9uuDV' },
             { name: 'tiktok', posted: true, url: 'https://www.tiktok.com/@eatlivewander/video/6956326890739223813' },
@@ -269,10 +355,6 @@ const data = [
                 price: '$20.00',
             },
         ],
-        postURL: 'https://www.tiktok.com/@eatlivewander/video/6956326890739223813',
-        imageURL:
-            'https://wndr.click/wp-content/uploads/2021/04/How-To-Cut-And-Maintain-Fresh-Flowers-And-Roses-For-A-Diptqyue-Baies-Black-Vase-small.jpg',
-        imageAlt: 'How to Cut and Maintain Roses In A Vase',
     },
 ];
 
